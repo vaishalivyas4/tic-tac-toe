4b8a2c19664d5b447dc76929f906649129dc9f3b
Vaishali Vyas <vaishalivyas4@users.noreply.github.com>
2020-06-27 15:08:56 +0530

    Commit#1

---
+++ Tic-Tac-Toe/Assets/Scripts/GameController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text[] buttonList;
+    private string playerSide;
+
+    void Awake()
+    {
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+    }
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        //FIRST ROW//
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //SECOND ROW//
+        else if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //THIRD ROW//
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //FIRST COLUMN//
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //SECOND COLUMN//
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //THIRD COLUMN//
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //LEFT DIAGONAL//
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        //RIGHT DIAGONAL//
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver();
+            Debug.Log("Current Player Wins!");
+        }
+
+        ChangeSides();
+    }
+
+    void GameOver()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+    }
+}
---
+++ Tic-Tac-Toe/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    GameObject[] buttonTextGOs;
+
+    private GameController gameController;
+
+    void Start()
+    {
+        buttonTextGOs = GameObject.FindGameObjectsWithTag("ButtonText");
+    }
+
+    public void SetSpace(int index)
+    {
+        buttonTextGOs[index].GetComponent<Text>().text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+}
